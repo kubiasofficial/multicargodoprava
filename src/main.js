3d82c23b59fdd3627262ff15644132df18efbeaf
kubiasofficial <suslik.kuba@seznam.cz>
2025-08-26 12:09:29 +0200

    Implement code changes to enhance functionality and improve performance

--- src/main.js
+++ src/main.js
@@ -4,23 +4,29 @@ const pageTitle = document.querySelector('.page-title');
 const pageContent = document.getElementById('page-content');
 const navBtns = document.querySelectorAll('.nav-btn');
 
+
 function setPage(page) {
+  const background = document.getElementById('background');
   switch(page) {
     case 'strojvedouci':
       pageTitle.textContent = 'Strojvedoucí';
       pageContent.innerHTML = '<h2 style="color:#fff;text-align:center;">Stránka Strojvedoucí zatím není hotová.</h2>';
+      background.style.background = "url('/Pictures/1185.png') center center/cover no-repeat";
       break;
     case 'vypravci':
       pageTitle.textContent = 'Výpravčí';
       pageContent.innerHTML = '<h2 style="color:#fff;text-align:center;">Stránka Výpravčí zatím není hotová.</h2>';
+      background.style.background = "url('/Pictures/1182.png') center center/cover no-repeat";
       break;
     case 'ridic':
       pageTitle.textContent = 'Řidič';
       pageContent.innerHTML = '<h2 style="color:#fff;text-align:center;">Stránka Řidič zatím není hotová.</h2>';
+      background.style.background = "url('/Pictures/1182.png') center center/cover no-repeat";
       break;
     default:
       pageTitle.textContent = 'Přehled';
       pageContent.innerHTML = '';
+      background.style.background = "url('/Pictures/1182.png') center center/cover no-repeat";
   }
 }
 
